75d382f26206ce0e42e4ce36ff92f4e5381bd4d1
Jairo Davila <jdavilaco@gmail.com>
2023-12-08 09:52:46 -0600

    feat: Exercise 6.9

--- anecdotes/src/App.jsx
+++ anecdotes/src/App.jsx
@@ -1,10 +1,12 @@
 import AnecdoteForm from './components/AnecdoteForm'
 import AnexdoteList from './components/AnecdoteList'
+import Filter from './components/ContentFilter'
 
 const App = () => {
   return (
     <div>
       <h2>Anecdotes</h2>
+      <Filter />
       <AnexdoteList />
       <h2>Create new</h2>
       <AnecdoteForm />
--- anecdotes/src/components/AnecdoteList.jsx
+++ anecdotes/src/components/AnecdoteList.jsx
@@ -3,8 +3,14 @@ import { addVote } from '../reducers/anecdoteReducer'
 
 const Anecdotes = () => {
   const dispatch = useDispatch()
-  const anecdotes = useSelector(state => state)
 
+  const anecdotes = useSelector(({ filter, anecdotes }) => {
+    if ( filter === '' ) {
+      return anecdotes
+    }
+    return anecdotes.filter(anecdote => anecdote.content.toLowerCase().includes(filter.toLowerCase()))
+  })
+  
   return(
     <div>
       {anecdotes.sort((a, b) => b.votes - a.votes)
