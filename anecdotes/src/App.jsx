291c92c5cf7f933057ac747d22c936428a67d0db
Jairo Davila <jdavilaco@gmail.com>
2024-01-31 19:28:29 -0600

    feat: Exercises module 6d 6.20-6.22

--- anecdotes/src/App.jsx
+++ anecdotes/src/App.jsx
@@ -3,7 +3,6 @@ import NewAnecdote from './components/AnecdoteForm'
 import Anecdotes from './components/AnecdoteList'
 import Filter from './components/ContentFilter'
 import Notification from './components/Notification'
-import anecdoteService from './services/anecdotes'
 import { initializeAnecdotes } from './reducers/anecdoteReducer'
 import { useDispatch } from 'react-redux'
 
